59bde089804935288f27d565b26a2f823f99aed9
M Fachmi Firmansyah <firmansyahfachmi5@gmail.com>
2021-01-08 18:14:46 +0700

    setup locales and create multilanguage

---
+++ assets/js/jquery.MultiLanguage.js
@@ -0,0 +1,51 @@
+// Language JSON File Location
+// localStorage.setItem('language', 'en');
+var language = localStorage.getItem('language');
+// Default Language
+var default_lang = 'en';
+let lang = localStorage.getItem('language');
+// Set Selected Language
+function setLanguage() {
+    
+    if(lang == 'en'){
+        lang = 'id'
+    } else if(lang == 'id'){
+        lang = 'en'
+    }
+    localStorage.setItem('language', lang);
+    language = localStorage.getItem('language');
+    // Run Multi Language Plugin
+    getLanguage()
+}
+
+// Run Multi Language Plugin
+function getLanguage() {
+    // Language on user preference
+    (language == null) ? setLanguage(default_lang) : false;
+    // Load data of selected language
+    $.ajax({
+        url: 'locales/' + language + '.json',
+        dataType: 'json', async: true
+    }).done(function (lang) {
+        // add selected language class to the body tag
+        // $('body').attr('class', language);
+        // Loop through message in data
+        if(localStorage.getItem('language') == 'en'){
+            $('.footer-sound').html('EN');
+        } else if(localStorage.getItem('language') == 'id'){
+            $('.footer-sound').html('ID');
+        }
+        $.each(lang, function (index, val) {
+            // (index === 'head') ? $(document).attr("title", val['title']) : false;
+            $(index).children().each(function () {
+                $(this).text(val[$(this).attr('key')])
+            })
+        })
+    })
+}
+
+// Auto Loader
+$(document).ready(function () {
+    if (language != null && language !== default_lang)
+        getLanguage(language);
+});
