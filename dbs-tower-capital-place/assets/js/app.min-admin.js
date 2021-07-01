83e0951de006bf45209c4757b4064bf7fea590b0
M Fachmi Firmansyah <firmansyahfachmi5@gmail.com>
2021-07-01 18:42:20 +0700

    fixing endpoint

--- dbs-tower-capital-place/assets/js/app.min-admin.js
+++ dbs-tower-capital-place/assets/js/app.min-admin.js
@@ -175,7 +175,7 @@ function pilihData(e) {
 	var str_quis = '';
 	$.ajax({
         method:'POST',
-        url: baseUrlCms+'/backend/api/user/getFloor',
+        url: baseUrlCms+'/backend/api/user/getFloor?location=capital',
         data: 'info_lantai='+e,
         dataType: "json",
         beforeSend: function(xhr) {
